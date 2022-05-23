8dda67d7b8217f04cd035bbd486255bc1915dd2d
Dimitri Bertolami <dimibertolami@gmail.com>
2022-05-23 14:41:51 +0200

    display data in table is not working at the moment

--- pokedex.js
+++ pokedex.js
@@ -5,7 +5,7 @@
 *
 *
 * */
-document.getElementById("searchButton").addEventListener("click", function() {
+document.getElementById("searchButton").addEventListener("click", function () {
     document.getElementById("abilities").innerHTML = "";
     document.getElementById("moves").innerHTML = "";
     getName(document.getElementById("Name").value);
@@ -19,35 +19,75 @@ function displayPokemon(data) {
     const moves = data.moves;
 
     document.getElementById("image").src = pokemonImg;
+    // let row = document.getElementById("poketable").insertRow(-1);
+    // let cell1 = row.insertCell(0);
+    // let cell2 = row.insertCell(1);
+    // let cell3 = row.insertCell(2);
+    // let cell4 = row.insertCell(3);
     DisplayData("id", pokemonID);
-    DisplayData("pokemonName", pokemonName);
+    // cell1.innerHTML = pokemonID;
     // alert(abilities.length);
 
-    // loopAbilities;
-    for (let i = 0; i < abilities.length; i++) {
-        let abilty = abilities[i].ability.name;
-        console.log(i);
-        console.log(abilty);
-        DisplayData("abilities", abilty, false);
-    }
-    // loopmoves;
-    for (let i = 0; i < moves.length; i++) {
-        let move = moves[i].move.name;
-        console.log(i);
-        console.log(move);
-        DisplayData("moves", move, false);
+    // if abilities.length is larger than moves.length then run for loop that many times and check if the other value is empty
+    if (abilities.length > moves.length) {
+        // loopAbilities;
+        for (let i = 0; i < abilities.length; i++) {
+            let abilty = abilities[i].ability.name;
+            let move = moves[i].move.name;
+            console.log(i);
+            console.log(abilty);
+
+            DisplayData("abilities", abilty, false);
+            let row = document.getElementById("poketable").insertRow(-1);
+            let cell1 = row.insertCell(0);
+            let cell2 = row.insertCell(1);
+            let cell3 = row.insertCell(2);
+            cell1.innerHTML = pokemonID;
+            cell2.innerHTML += abilty;
+            cell3.innerHTML += move;
+        }
+    } else {
+        // loopmoves;
+        for (let i = 0; i < moves.length; i++) {
+            let abilty = abilities[i].ability.name;
+            let move = moves[i].move.name;
+            console.log(i);
+            console.log(move);
+            DisplayData("moves", move, false);
+            let row = document.getElementById("poketable").insertRow(-1);
+            let cell1 = row.insertCell(0);
+            let cell2 = row.insertCell(1);
+            let cell3 = row.insertCell(2);
+            cell1.innerHTML = pokemonID;
+            cell2.innerHTML += abilty;
+            cell3.innerHTML += move;
+        }
     }
 }
 
-function DisplayData(elementID, data, dataFlush = true){
-    if(dataFlush){document.getElementById(elementID).innerHTML = ""}
-    element = document.getElementById(elementID);
+function addRowData(cell = cell2, ID, abilitiy, move) {
+    let row = document.getElementById("poketable").insertRow(-1);
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
+    let cell3 = row.insertCell(2);
+    let cell4 = row.insertCell(3);
+
+    cell1.innerHTML = "";
+    cell2.innerHTML = data;
+    cell3.innerHTML = "";
+    cell4.innerHTML = "";
+
+}
+
+function DisplayData(elementID, data, dataFlush = true) {
+    // if(dataFlush){document.getElementById(elementID).innerHTML = ""}
+    let element = document.getElementById(elementID);
     h1 = document.createElement("h1");
     h1.innerHTML = data;
     element.appendChild(h1);
 }
 
-function getName(name = "pikachu"){
+function getName(name = "pikachu") {
     fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
         .then((response) => {
             if (response.ok) {
