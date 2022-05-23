b06c6cd5ebf4f1500a93ca6003909f30385ca1a6
Dimitri Bertolami <dimibertolami@gmail.com>
2022-05-23 10:59:22 +0200

    basics are working (image, pokemonID, name and abilities... but only two are for now hardcoded)

--- pokedex.js
+++ pokedex.js
@@ -1,5 +1,28 @@
-getName("pikachu")
+// getName("pikachu")
+document.getElementById("searchButton").addEventListener("click", RunSearch);
 
+function RunSearch(){
+    getName(document.getElementById("Name").value);
+}
+
+
+function displayPokemon(data) {
+    const pokemonImg = data.sprites.front_default;
+    const pokemonID = data.id;
+    const pokemonName = data.name;
+    const abilities = `${data.abilities[0].ability.name}, ${data.abilities[1].ability.name}`;
+    document.getElementById("image").src = pokemonImg;
+    DisplayData("id", pokemonID);
+    DisplayData("pokemonName", pokemonName);
+    DisplayData("abilities", abilities);
+}
+
+function DisplayData(elementID, data){
+    element = document.getElementById(elementID);
+    h1 = document.createElement("h1");
+    h1.innerHTML = data;
+    element.appendChild(h1);
+}
 
 function getName(name = "pikachu"){
     fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
@@ -15,18 +38,4 @@ function getName(name = "pikachu"){
             displayPokemon(data);
         })
         .catch((error) => console.error("FETCH ERROR:", error));
-
-    function displayPokemon(data) {
-        const pokemonID = data.id;
-        const pokemonName = data.name;
-        DisplayData("id", pokemonID);
-        DisplayData("pokemonName", pokemonName);
-    }
-}
-
-function DisplayData(elementID, data){
-    element = document.getElementById(elementID);
-    h1 = document.createElement("h1");
-    h1.innerHTML = data;
-    element.appendChild(h1);
 }
