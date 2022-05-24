1e39be0b121d2a29b68da5b7c47161c7c10fca91
Dimitri Bertolami <dimibertolami@gmail.com>
2022-05-24 23:45:49 +0200

    adding pokeball sunk my battleship.. before going into class tomorow I first needed to sort that out

--- pokedex.js
+++ pokedex.js
@@ -3,18 +3,27 @@ let action = "go";
 document.getElementById("pokeball__button").addEventListener("click", function () {
     let table = document.getElementById("poketable");
     let bodyRef = table.getElementsByTagName('tbody')[0];
-    console.log(bodyRef);
-    console.log(table);
+
     bodyRef.innerHTML = '';
-    if(action==="go"){
-        getName(document.getElementById("Name").value);
+        if(action==="go"){
+        getName(document.getElementById("Name").value)
+        document.getElementById("pokeball__button").innerHTML = "Go";
         action="reload";
     } else {
         action="go";
         location.reload();
     }
 });
-
+function displayEvolution(data2){
+    try {
+        const evolve1 = data2.chain.evolves_to[0].species.name;
+        const evolve2 = data2.chain.evolves_to[1].species.name;
+        alert(evolve1);
+        alert(evolve2);
+    } catch (err) {
+        // error handling
+    }
+}
 function displayPokemon(data) {
     const pokemonImg = data.sprites.front_default;
     const pokemonID = data.id;
@@ -22,41 +31,41 @@ function displayPokemon(data) {
     const abilities = data.abilities;
     const moves = data.moves;
 
-    document.getElementById("image").src = pokemonImg;
+    document.getElementById("image1").src = pokemonImg;
     let table = document.getElementById("poketable");
-    let bodyRefByTag = table.getElementsByTagName('tbody')[0];
-    // console.log(bodyRefByTag);
+    let bodyRefByTag = table.getElementsByClassName('table table-hover')[0];
+
     let body = table.createTBody();
     let row = body.insertRow();
-    // console.log(abilities.length + " " + moves.length);
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
+    let cell3 = row.insertCell(2);
+    let cell4 = row.insertCell(3);
+
     const arrLongest = (abilities.length > moves.length) ? abilities.length : moves.length;
     for (let i = 0; i < arrLongest; i++) {
         row = body.insertRow();
-        let cell1 = row.insertCell(0);
-        let cell2 = row.insertCell(1);
-        let cell3 = row.insertCell(2);
-        let cell4 = row.insertCell(3);
+        cell1 = row.insertCell(0);
+        cell2 = row.insertCell(1);
+        cell3 = row.insertCell(2);
+        cell4 = row.insertCell(3);
         try {
-            if(i>0){
-                console.log(`${i})    ${pokemonName}`);
-                cell1.innerHTML = `${i+1})    ${pokemonName}`;
+            if(!i>0){
+                cell1.innerHTML = `${pokemonID} ${pokemonName}`;
             } else {
-                console.log(`${i})    ${pokemonID}`);
-                cell1.innerHTML = `${i+1})    ${pokemonID}`;
+                cell1.innerHTML = ``;
             }
         } catch (err) {
             // error handling
         }
         try {
             let ability = abilities[i].ability.name;
-            console.log(`${i}) ability: (${ability})`);
             cell2.innerHTML = ability;
         } catch (err) {
             // error handling
         }
         try {
             let move = moves[i].move.name;
-            console.log(`${i}) move: (${move})`);
             cell3.innerHTML = move;
         } catch (err) {
             // error handling
@@ -76,7 +85,26 @@ function getName(name = "pikachu") {
         .then(data => {
             console.log(data);
             displayPokemon(data);
+            // alert(data.id);
+            getEvolution(data.id);
         })
         // .catch((error) => console.error("FETCH ERROR:", error));
         .catch((error) => console.error("FETCH ERROR:", error));
+}
+
+function getEvolution(id = 1){
+        fetch(`https://pokeapi.co/api/v2/evolution-chain/${id}`)
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error("NETWORK RESPONSE ERROR");
+                }
+            })
+            .then(data2 => {
+                console.log(data2);
+                displayEvolution(data2);
+            })
+            // .catch((error) => console.error("FETCH ERROR:", error));
+            .catch((error) => console.error("FETCH ERROR:", error));
 }
