35a42b0c06b5f1d03ae0870ec6639571b5a957d5
Dimitri Bertolami <dimibertolami@gmail.com>
2022-05-24 16:52:57 +0200

    positioning css is hell

--- pokedex.js
+++ pokedex.js
@@ -1,18 +1,27 @@
+let action = "go";
+
 document.getElementById("searchButton").addEventListener("click", function () {
     let table = document.getElementById("poketable");
     let bodyRef = table.getElementsByTagName('tbody')[0];
     console.log(bodyRef);
     console.log(table);
     bodyRef.innerHTML = '';
-    getName(document.getElementById("Name").value);
-    document.getElementById("searchButton").disabled = true;
-    document.getElementById("NewSearch").disabled = false;
+    if(action==="go"){
+        getName(document.getElementById("Name").value);
+        document.getElementById("searchButton").textContent = "Reload";
+        action="reload";
+    } else {
+        document.getElementById("searchButton").textContent = "Go";
+        action="go";
+        location.reload();
+    }
+    // document.getElementById("searchButton").disabled = true;
+    // document.getElementById("NewSearch").disabled = false;
 });
-document.getElementById("NewSearch").addEventListener("click", function () {
-    document.getElementById("searchButton").disabled = false;
-    document.getElementById("NewSearch").disabled = true;
-    location.reload();
-})
+// document.getElementById("NewSearch").addEventListener("click", function () {
+//     document.getElementById("searchButton").disabled = false;
+//     document.getElementById("NewSearch").disabled = true;
+// })
 function displayPokemon(data) {
     const pokemonImg = data.sprites.front_default;
     const pokemonID = data.id;
@@ -23,10 +32,10 @@ function displayPokemon(data) {
     document.getElementById("image").src = pokemonImg;
     let table = document.getElementById("poketable");
     let bodyRefByTag = table.getElementsByTagName('tbody')[0];
-    console.log(bodyRefByTag);
+    // console.log(bodyRefByTag);
     let body = table.createTBody();
     let row = body.insertRow();
-    console.log(abilities.length + " " + moves.length);
+    // console.log(abilities.length + " " + moves.length);
     const arrLongest = (abilities.length > moves.length) ? abilities.length : moves.length;
     for (let i = 0; i < arrLongest; i++) {
         row = body.insertRow();
@@ -36,18 +45,17 @@ function displayPokemon(data) {
         let cell4 = row.insertCell(3);
         try {
             if(i>0){
-                console.log(`${pokemonName}`);
-                cell1.innerHTML = `${pokemonName}`;
+                console.log(`${i})    ${pokemonName}`);
+                cell1.innerHTML = `${i+1})    ${pokemonName}`;
             } else {
-                console.log(`${pokemonID}`);
-                cell1.innerHTML = `${pokemonID}`;
+                console.log(`${i})    ${pokemonID}`);
+                cell1.innerHTML = `${i+1})    ${pokemonID}`;
             }
         } catch (err) {
             // error handling
         }
         try {
             let ability = abilities[i].ability.name;
-            console.log(i);
             console.log(`${i}) ability: (${ability})`);
             cell2.innerHTML = ability;
         } catch (err) {
@@ -55,8 +63,7 @@ function displayPokemon(data) {
         }
         try {
             let move = moves[i].move.name;
-            console.log(i);
-            console.log(`move: (${move})`);
+            console.log(`${i}) move: (${move})`);
             cell3.innerHTML = move;
         } catch (err) {
             // error handling
